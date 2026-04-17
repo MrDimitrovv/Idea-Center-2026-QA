0168ced26785166ab4c1c76994df229c37cae453
Martin DImitrov <mr.iv.dimitrov@gmail.com>
2026-04-18 01:47:21 +0300

    Added Models

---
+++ Models/ApiResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Text.Json.Serialization;
+
+
+namespace Exam_Prep.Models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+        public string? Msg { get; set; }
+
+        [JsonPropertyName("id")]
+
+        public string? Id { get; set; }
+
+    }
+}
---
+++ Models/IdeaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace Exam_Prep.Models
+{
+    internal class IdeaDTO
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+    }
+}
